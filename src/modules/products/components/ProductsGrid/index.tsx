2d53d8cf082f726f28c875f11b8b48f14136b724
joshua31082002 <joshua.sahayam@gmail.com>
2026-02-18 02:36:10 +0530

    Added loading state for the sample query

--- src/modules/products/components/ProductsGrid/index.tsx
+++ src/modules/products/components/ProductsGrid/index.tsx
@@ -1,16 +1,28 @@
 import { useProductsQuery } from '@/modules/products/hooks/useProductsQuery';
 import styles from './ProductsGrid.module.scss';
 import ProductCard from '@/modules/products/components/ProductCard';
+import ConditionalRender from '@/shared/components/ui/ConditionalRender';
+import Spin from '@/shared/components/ui/Spin';
 
 const ProductsGrid = () => {
-  const { data: products } = useProductsQuery();
+  const { data: products, isLoading } = useProductsQuery();
 
   return (
-    <div className={styles['grid-container']}>
-      {products?.map((product) => (
-        <ProductCard product={product} />
-      ))}
-    </div>
+    <ConditionalRender
+      condition={isLoading}
+      children={
+        <div className={styles['loader']}>
+          <Spin size='large' />
+        </div>
+      }
+      fallback={
+        <div className={styles['grid-container']}>
+          {products?.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </div>
+      }
+    />
   );
 };
 
